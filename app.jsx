ecac6984e595f82562633350205eb15ab5759ebf
jhingabao14 <jhingabao14@gmail.com>
2020-03-29 20:50:37 +0800

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import socketIOClient from 'socketIOClient';
+
+class Title extends React.Component {
+  render() {
+    return (<h1 className="">{this.props.label}</h1>)
+  }
+}
+
+class Paragraph extends React.Component {
+  render() {
+    return (
+      <p className="">
+      {this.props.text}
+      </p>
+    )
+  }
+}
+
+class Main extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return  (
+      <div className="">
+        <Title label={this.props.title} />
+          <Paragraph text={this.props.text} />
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <Main title="React" text="Experience is something you get just after you need it."></Main>,
+  document.getElementById('react-app')
+);
