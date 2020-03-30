865cf1bc7a4808ff21a4b24d4c434ae95c05fd53
jhingabao14 <jhingabao14@gmail.com>
2020-03-31 00:25:01 +0800

    Add spring and react gesture

--- app.jsx
+++ app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as SocketIOClient from 'socket.io-client';
+//import { SocketClient } from 'socket.io-client';
+import * as io from 'socket.io-client';
+import { useDrag } from 'use-react-gesture';
+import { Spring } from 'react-spring';
 
 class Title extends React.Component {
   render() {
@@ -24,8 +27,18 @@ class Main extends React.Component {
     this.state = {};
   }
   componentDidMount() {
-    //console.log('pass', SocketIOClient.SocketClient)
-    //const socket = SocketIOClient.SocketClient("127.0.0.1:5000");
+    console.log(useDrag, Spring)
+    const socket = io.connect('http://localhost:5000')
+    socket.on('connection', () => console.log('connected'))
+    /*const socket = new SocketClient("http://127.0.0.1:5000").open()
+    /socket.on("connect", (request) => {
+        console.log('connected')
+         request.sendMessage("message")
+    })
+     socket.on("socketReady", (request) => {
+        console.log('connected 2')
+         request.sendMessage("message")
+    })*/
   }
 
   render() {
--- config.js
+++ config.js
@@ -13,7 +13,10 @@ SystemJS.config({
     'systemjs-babel-build': 'systemjs-plugin-babel@latest/systemjs-babel-browser.js',
     'react': 'react@16.4.2/umd/react.production.min.js',
     'react-dom': 'react-dom@16.4.2/umd/react-dom.production.min.js', 
-    'socket.io-client': 'jomnis-socket-client@0.4.2/dist/lib/SocketClient.js'
+    'socket.io-client2': 'jomnis-socket-client@0.4.2/dist/lib/SocketClient.js', 
+    'socket.io-client' :'socket.io-client@2.3.0/dist/socket.io.js',
+    'use-react-gesture': 'react-use-gesture@6.0.14/dist/react-use-gesture.cjs.production.min.js', 
+    'react-spring': 'react-spring-loaders@1.0.1/dist/index.js'
   },
   transpiler: 'plugin-babel'
 });
