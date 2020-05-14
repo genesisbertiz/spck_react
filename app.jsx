b74cf644f464e63efff097adc17328e670103a8a
jhingabao14 <jhingabao14@gmail.com>
2020-05-14 18:56:23 +0800

    Add easy-peasy

--- app.jsx
+++ app.jsx
@@ -4,55 +4,52 @@ import ReactDOM from 'react-dom';
 import * as io from 'socket.io-client';
 import { useDrag } from 'use-react-gesture';
 import { Spring } from 'react-spring';
+import * as styled from 'styled-components'
+import { createStore, action, StoreProvider, useStoreActions } from 'easy-peasy';
+//import * as useMeasure from 'react-use';
 
-class Title extends React.Component {
-  render() {
-    return (<h1 className="">{this.props.label}</h1>)
-  }
-}
-
-class Paragraph extends React.Component {
-  render() {
-    return (
-      <p className="">
-      {this.props.text}
-      </p>
-    )
-  }
-}
-
-class Main extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-    console.log(useDrag, Spring)
-    const socket = io.connect('http://localhost:5000')
-    socket.on('connection', () => console.log('connected'))
-    /*const socket = new SocketClient("http://127.0.0.1:5000").open()
-    /socket.on("connect", (request) => {
-        console.log('connected')
-         request.sendMessage("message")
+const store = createStore({
+  todos: {
+    items: ['Create store', 'Wrap application', 'Use store'],
+    add: action((state, payload) => {
+      console.log(state, 'state')
+      state.items.push(payload)
     })
-     socket.on("socketReady", (request) => {
-        console.log('connected 2')
-         request.sendMessage("message")
-    })*/
   }
-
-  render() {
-    return  (
-      <div className="">
-        <Title label={this.props.title} />
-          <Paragraph text={this.props.text} />
-      </div>
-    )
+});
+const App = styled.div`
+  color: red;
+  ul.list-buttons, ul li {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
   }
-}
+  ul li {
+    display: flex;
+  }
+`
 
+function Main() {
+  const add = useStoreActions(actions => actions.add);
+  const alphabet = [...Array(26)].map((q,w)=>String.fromCharCode(w+97));
+  
+  const inputs = [];
+  return <App>
+  <button onClick={() => {
+    console.log('press')
+    add()
+  }}>test</button>
+    <ul className='list-buttons'>
+      { alphabet.map((e, i) => {
+        return <li>{e}{i + 1}</li>
+      })}
+     </ul>
+  </App> 
+} 
 
 ReactDOM.render(
-  <Main title="React" text="Experience is something you get just after you need it."></Main>,
+  <StoreProvider store={store}>
+  <Main></Main>
+  </StoreProvider>,
   document.getElementById('react-app')
 );
--- config.js
+++ config.js
@@ -11,12 +11,15 @@ SystemJS.config({
   map: {
     'plugin-babel': 'systemjs-plugin-babel@latest/plugin-babel.js',
     'systemjs-babel-build': 'systemjs-plugin-babel@latest/systemjs-babel-browser.js',
-    'react': 'react@16.4.2/umd/react.production.min.js',
-    'react-dom': 'react-dom@16.4.2/umd/react-dom.production.min.js', 
+    'react': 'react@16.13.1/umd/react.production.min.js',
+    'react-dom': 'react-dom@16.13.1/umd/react-dom.production.min.js', 
     'socket.io-client2': 'jomnis-socket-client@0.4.2/dist/lib/SocketClient.js', 
     'socket.io-client' :'socket.io-client@2.3.0/dist/socket.io.js',
     'use-react-gesture': 'react-use-gesture@6.0.14/dist/react-use-gesture.cjs.production.min.js', 
-    'react-spring': 'react-spring-loaders@1.0.1/dist/index.js'
+    'react-spring': 'react-spring-loaders@1.0.1/dist/index.js', 
+    'styled-components':'styled-components@4.3.2/dist/styled-components.min.js', 
+    'react-use':'react-use@14.1.1', 
+    'easy-peasy': 'easy-peasy@3.2.6'
   },
   transpiler: 'plugin-babel'
 });
